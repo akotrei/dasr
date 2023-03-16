91579b3c0618129496f56fe559ac92dff73b7817
Andrei Kot <akotrei@gmail.com>
2023-03-17 02:42:55 +0300

    4

--- include/utils/cmp_std.h
+++ include/utils/cmp_std.h
@@ -4,6 +4,6 @@
 int dast_cmp_ptr_std(void* o1, void* o2);
 int dast_cmp_int_std(void* o1, void* o2);
 int dast_cmp_float_std(void* o1, void* o2);
-int dast_cmp_str_ascii_std(void* o1, void* o2); /* ??? */
+int dast_cmp_str_ascii_std(void* o1, void* o2);
 
 #endif /* __DAST_CMP_STD_H__ */
--- include/utils/string_std.h
+++ include/utils/string_std.h
@@ -1,5 +1,8 @@
 #ifndef __DAST_STRING_STD_H__
 #define __DAST_STRING_STD_H__
 
+void cmp
+unsigned long len
+
 
 #endif /* __DAST_STRING_STD_H__ */
